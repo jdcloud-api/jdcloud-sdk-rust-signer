dd57064f8d932130d48af62272cf3ec774aba5ea
lidaobing <lidaobing@jd.com>
2019-02-02 13:53:33 +0800

    add Credential.is_valid

--- src/credential.rs
+++ src/credential.rs
@@ -10,6 +10,10 @@ impl Credential {
             sk
         }
     }
+
+    pub fn is_valid(&self) -> bool {
+        !self.ak.is_empty() && !self.sk.is_empty()
+    }
 }
 
 #[cfg(test)]
@@ -21,5 +25,13 @@ mod tests {
         let ak = "1";
         let sk = "2";
         let _c = Credential::new(ak.to_string(), sk.to_string());
+    }
+
+    #[test]
+    fn test_is_valid() {
+        assert!(Credential::new("a".to_string(), "b".to_string()).is_valid());
+        assert!(Credential::new("a".to_string(), "".to_string()).is_valid());
+        assert!(Credential::new("".to_string(), "b".to_string()).is_valid());
+        assert!(Credential::new("".to_string(), "".to_string()).is_valid());
     }
 }
