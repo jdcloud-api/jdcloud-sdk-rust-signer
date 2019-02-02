6d49f312a8f67c15a0e18cbd78138e88fc7561c3
lidaobing <lidaobing@jd.com>
2019-02-02 14:46:58 +0800

    add sha2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 extern crate http;
+extern crate crypto;
 
 mod credential;
 
+use crypto::sha2::Sha256;
+use crypto::digest::Digest;
 use crate::credential::Credential;
-use http::{Request, Error};
+use http::Request;
+
+static EMPTY_STRING_SHA256: &str = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
 
 pub struct JdcloudSigner {
     credential: Credential,
@@ -20,8 +26,21 @@ impl JdcloudSigner {
         }
     }
 
-    pub fn sign_request(&self, request: Request<()>) -> Result<Request<()>, Error> {
-        Ok(Request::builder().body(()).unwrap())
+    pub fn sign_request(&self, request: Request<String>) -> Result<Request<String>, &str> {
+        if !self.credential.is_valid() {
+            panic!("invalid credential")
+        }
+        Ok(Request::builder().body("".to_string()).unwrap())
+    }
+
+    fn compute_payload_hash(&self, request: Request<String>) -> String {
+        if request.body().is_empty() {
+            EMPTY_STRING_SHA256.to_string()
+        } else {
+            let mut hasher = Sha256::new();
+            hasher.input_str(request.body());
+            hasher.result_str()
+        }
     }
 }
 
@@ -39,7 +58,7 @@ mod tests {
             .method("GET")
             .header(CONTENT_TYPE, "application/json")
             .header(USER_AGENT, "JdcloudSdkRust/0.0.1 vm/0.7.4")
-            .body(())
+            .body("".to_string())
             .unwrap();
         let req = s.sign_request(req);  
     }
