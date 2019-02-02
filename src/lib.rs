f3d6fdd8439f002b7578bbcdfbc0ed98c7b3b9a6
lidaobing <lidaobing@jd.com>
2019-02-02 15:11:56 +0800

    add chrono

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate http;
 extern crate crypto;
+extern crate chrono;
 
 mod credential;
 
@@ -7,9 +8,13 @@ use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 use crate::credential::Credential;
 use http::Request;
+use http::header::HeaderValue;
+use chrono::prelude::*;
 
 static EMPTY_STRING_SHA256: &str = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-
+static LONG_DATE_FORMAT_STR: &str = "%Y%m%dT%H%M%SZ";
+static DATE_HEADER: &str = "x-jdcloud-date";
+static NONCE_HEADER: &str = "x-jdcloud-nonce";
 
 pub struct JdcloudSigner {
     credential: Credential,
@@ -26,14 +31,24 @@ impl JdcloudSigner {
         }
     }
 
-    pub fn sign_request(&self, request: Request<String>) -> Result<Request<String>, &str> {
+    pub fn sign_request(&self, request: &Request<String>) -> Result<Request<String>, &str> {
         if !self.credential.is_valid() {
-            panic!("invalid credential")
+            panic!("invalid credential");
         }
+        let payload_hash = self.compute_payload_hash(request);
+        let utc: DateTime<Utc> = Utc::now();
+        let utc = utc.format(LONG_DATE_FORMAT_STR).to_string();
+
+        let mut res = Request::builder();
+        res.header(DATE_HEADER, HeaderValue::from_str(&utc).unwrap());
+        //res.header(NONCE_HEADER, self.gen_uuid_header());
+        // string dateHeaderValue = now.ToGmtString(LONG_DATE_FORMAT_STR);
+
+
         Ok(Request::builder().body("".to_string()).unwrap())
     }
 
-    fn compute_payload_hash(&self, request: Request<String>) -> String {
+    fn compute_payload_hash(&self, request: &Request<String>) -> String {
         if request.body().is_empty() {
             EMPTY_STRING_SHA256.to_string()
         } else {
@@ -42,6 +57,8 @@ impl JdcloudSigner {
             hasher.result_str()
         }
     }
+
+
 }
 
 #[cfg(test)]
@@ -60,6 +77,6 @@ mod tests {
             .header(USER_AGENT, "JdcloudSdkRust/0.0.1 vm/0.7.4")
             .body("".to_string())
             .unwrap();
-        let req = s.sign_request(req);  
+        let req = s.sign_request(&req);  
     }
 }
